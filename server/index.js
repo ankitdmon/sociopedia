8d03dcd24f52c23a8277c658ae8a1ae68f280ab7
Ankit Mishra <ankitmishra2108@gmail.com>
2022-12-25 02:32:20 +0530

    User Routes Setup

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
 import { register } from './controllers/auth.js';
 import { verifyToken } from './middleware/auth.js';
 
@@ -42,7 +43,9 @@ const upload = multer({ storage });
 app.post('/auth/register', upload.single('picture'), register);
 
 /* Routes */
-app.post("/auth", authRoutes);
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+// app.use('/posts', postRoutes);
 
 /* MONGOOS setup */
 const PORT = process.env.PORT || 6001;
