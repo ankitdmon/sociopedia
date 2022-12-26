5be3ea03b4ba9ff59f8bb07976ea208c924cf796
Ankit Mishra <ankitmishra2108@gmail.com>
2022-12-26 22:53:54 +0530

    NavBar

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { Box } from '@mui/system';
+import React from 'react';
+import Navbar from 'scenes/navbar';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
-}
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
+};
 
 export default HomePage;
